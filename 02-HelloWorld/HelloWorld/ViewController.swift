56556b0091a822deadbd43c297538c0a3cb19499
Zizi_Kim <rlarjsdn3@naver.com>
2023-02-19 23:40:40 +0900

    02-HelloWorld 프로젝트 추가

---
+++ 02-HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 김건우 on 2023/02/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var lblHello: UILabel!
+    @IBOutlet var txtName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnSend(_ sender: UIButton) {
+        lblHello.text = "Hello, " + txtName.text!
+    }
+}
+
