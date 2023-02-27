81fcde3c9890b2325014f1c0988b7f832d4c004d
Zizi_Kim <rlarjsdn3@naver.com>
2023-02-27 14:30:47 +0900

    Tab 프로젝트 추가

---
+++ 10-Tab( Mission)/Tab/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Tab
+//
+//  Created by 김건우 on 2023/02/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnMoveImageView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 1
+    }
+    
+    @IBAction func btnMoveDatePickerView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 2
+    }
+}
+
